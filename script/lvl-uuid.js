f75959df1d90ab38cd65c2fa162587c7f6c8a6ac
Elisha Shapiro <elishas@sinapsa.co.il>
2014-02-28 10:49:15 +0200

    Refactored the uuid service, to be more efficient.

--- script/lvl-uuid.js
+++ script/lvl-uuid.js
@@ -7,19 +7,19 @@ try {
 }
 
 module.factory('uuid', function () {
-	var svc = {
-		new: function () {
-			function _p8(s) {
-				var p = (Math.random().toString(16) + "000000000").substr(2, 8);
-				return s ? "-" + p.substr(0, 4) + "-" + p.substr(4, 4) : p;
-			}
+	var svc = {};
 
-			return _p8() + _p8(true) + _p8(true) + _p8();
-		},
+	function _p8(s) {
+		var p = (Math.random().toString(16) + "000000000").substr(2, 8);
+		return s ? "-" + p.substr(0, 4) + "-" + p.substr(4, 4) : p;
+	}
 
-		empty: function () {
-			return '00000000-0000-0000-0000-000000000000';
-		}
+	svc.new = function () {
+		return _p8() + _p8(true) + _p8(true) + _p8();
+	};
+
+	svc.empty = function () {
+		return '00000000-0000-0000-0000-000000000000';
 	};
 
 	return svc;
